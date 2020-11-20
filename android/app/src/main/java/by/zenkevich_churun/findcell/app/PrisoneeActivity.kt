995adcbb41f84dc7117a056a0dc32167273282f3
Charlie-Green <vadim.churun.emp@gmail.com>
2020-11-20 19:28:50 +0300

    Android: setup project

---
+++ android/app/src/main/java/by/zenkevich_churun/findcell/app/PrisoneeActivity.kt
@@ -0,0 +1,16 @@
+package by.zenkevich_churun.findcell.app
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/** This [Activity] combines screens that set up
+  * [Prisonee]'s schedule and profile. **/
+class PrisoneeActivity: AppCompatActivity(R.layout.prisonee_activity) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ...
+    }
+}
