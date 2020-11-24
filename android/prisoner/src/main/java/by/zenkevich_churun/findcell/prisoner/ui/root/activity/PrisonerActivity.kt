390123f81846b1bfc876b2601298db1b5ad10844
Charlie-Green <vadim.churun.emp@gmail.com>
2020-11-24 11:49:43 +0300

    Android: Add Snackbar and Behavior for it

--- android/prisoner/src/main/java/by/zenkevich_churun/findcell/prisoner/ui/root/activity/PrisonerActivity.kt
+++ android/prisoner/src/main/java/by/zenkevich_churun/findcell/prisoner/ui/root/activity/PrisonerActivity.kt
@@ -1,11 +1,17 @@
 package by.zenkevich_churun.findcell.prisoner.ui.root.activity
 
 import android.app.Activity
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import by.zenkevich_churun.findcell.prisoner.R
 import by.zenkevich_churun.findcell.core.entity.Prisoner
+import by.zenkevich_churun.findcell.prisoner.repo.SavePrisonerResult
+import by.zenkevich_churun.findcell.prisoner.ui.root.vm.PrisonerRootViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.prisoner_activity.*
 
 
 /** This [Activity] combines screens that set up
@@ -15,6 +21,21 @@ class PrisonerActivity: AppCompatActivity(R.layout.prisoner_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // ...
+
+        val vm = PrisonerRootViewModel.get(applicationContext, this)
+        vm.savePrisonerResultLD.observe(this, Observer { result ->
+            if(result == SavePrisonerResult.SUCCESS) {
+                notifySaveSuccess()
+                vm.notifySaveResultConsumed()
+            }
+        })
+    }
+
+
+    private fun notifySaveSuccess() {
+        Snackbar.make(cdltRoot, R.string.save_prisoner_success_msg, 2000).apply {
+            setTextColor(Color.WHITE)
+            show()
+        }
     }
 }
