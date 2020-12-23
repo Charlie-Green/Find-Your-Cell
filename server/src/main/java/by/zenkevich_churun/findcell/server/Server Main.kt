192af008c0f55f607e15724c525a2af20ba547ce
Charlie-Green <vadim.churun.emp@gmail.com>
2020-12-23 17:08:31 +0200

    Server: Able to run

---
+++ server/src/main/java/by/zenkevich_churun/findcell/server/Server Main.kt
@@ -0,0 +1,8 @@
+package by.zenkevich_churun.findcell.server
+
+import org.springframework.boot.runApplication
+
+
+fun main(args: Array<String>) {
+	runApplication<ServerApplication>()
+}
---
+++ server/src/main/java/by/zenkevich_churun/findcell/server/ServerApplication.kt
@@ -0,0 +1,7 @@
+package by.zenkevich_churun.findcell.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+open class ServerApplication
