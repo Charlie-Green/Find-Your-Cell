fb8495aba3c6cde9435e0a2d21496b6249b65b26
Charlie-Green <vadim.churun.emp@gmail.com>
2021-01-01 12:27:41 +0200

    Android: Add Delete Checkbox to ArestsAdapter

--- android/prisoner/src/main/java/by/zenkevich_churun/findcell/prisoner/ui/arest/fragm/ArestsAdapter.kt
+++ android/prisoner/src/main/java/by/zenkevich_churun/findcell/prisoner/ui/arest/fragm/ArestsAdapter.kt
@@ -1,6 +1,9 @@
 package by.zenkevich_churun.findcell.prisoner.ui.arest.fragm
 
+import android.animation.ValueAnimator
+import android.util.LayoutDirection
 import android.view.*
+import androidx.core.view.updatePadding
 import androidx.recyclerview.widget.RecyclerView
 import by.zenkevich_churun.findcell.entity.entity.Arest
 import by.zenkevich_churun.findcell.prisoner.R
@@ -14,6 +17,7 @@ class ArestsAdapter(
 ): RecyclerView.Adapter<ArestsAdapter.ArestViewHolder>() {
 
     private var arests = listOf<Arest>()
+    private var checkable = false
 
 
     class ArestViewHolder(
@@ -21,22 +25,84 @@ class ArestsAdapter(
         itemView: View
     ): RecyclerView.ViewHolder(itemView) {
 
+        private val grltRoot  = itemView.grltRoot
         private val txtvStart = itemView.txtvStart
         private val txtvEnd   = itemView.txtvEnd
         private val txtvJails = itemView.txtvJails
+        private val chbDelete = itemView.chbDelete
 
 
         init {
             itemView.setOnClickListener {
                 openSchedule()
             }
+
+            itemView.setOnLongClickListener {
+                // TODO: Do it via ViewModel
+                setCheckable(true)
+                true
+            }
         }
 
 
         fun bind(arest: Arest) {
             txtvStart.text = formatDate(arest.start)
-            txtvEnd.text = formatDate(arest.end)
+            txtvEnd.text   = formatDate(arest.end)
             txtvJails.text = ArestUiUtil.jailsText(arest.jails)
+        }
+
+        fun setCheckable(checkable: Boolean) {
+            val animatedTextViews =
+                if(isLayoutExpanded) sequenceOf(txtvEnd, txtvJails)
+                else sequenceOf(txtvStart, txtvEnd,txtvJails)
+
+            if(checkable) {
+                translateIn(animatedTextViews)
+            } else {
+                translateOut()
+            }
+        }
+
+
+        private val isLayoutExpanded: Boolean
+            get() = when(val columnCount = grltRoot.columnCount) {
+                2 -> false
+                4 -> true
+                else -> throw Error("Cannot determine layout for column count $columnCount")
+            }
+
+        private fun translateIn(
+            animatedViews: Sequence<View> ) {
+
+            val res = itemView.context.resources
+            val desiredTranslate = res
+                .getDimensionPixelSize(R.dimen.delete_arest_checkbox_width)
+                .times(-1f)
+
+            ValueAnimator.ofFloat(chbDelete.translationX, desiredTranslate)
+                .setDuration(2000L)
+                .apply { addUpdateListener { animer ->
+                    val translate = animer.animatedValue as Float
+                    chbDelete.translationX = translate
+
+                    val pad = -translate.toInt()
+                    for(v in animatedViews) {
+                        setEndPadding(v, pad)
+                    }
+
+                }}.start()
+        }
+
+        private fun translateOut() {
+            // TODO
+        }
+
+        private fun setEndPadding(target: View, padding: Int) {
+            if(target.layoutDirection == LayoutDirection.RTL) {
+                target.updatePadding(left = padding)
+            } else {
+                target.updatePadding(right = padding)
+            }
         }
 
 
@@ -65,9 +131,39 @@ class ArestsAdapter(
         holder.bind(arests[position])
     }
 
+    override fun onBindViewHolder(
+        holder: ArestViewHolder,
+        position: Int,
+        payloads: MutableList<Any> ) {
+
+        if(payloads.size == 1 &&
+            payloads[0] === PAYLOAD_CHECKABLE ) {
+
+            holder.setCheckable(checkable)
+
+        } else {
+            super.onBindViewHolder(holder, position, payloads)
+        }
+    }
+
+
+    var isCheckable: Boolean
+        get() { return checkable }
+        set(value) {
+            if(checkable != value) {
+                checkable = value
+                notifyItemRangeChanged(0, itemCount, PAYLOAD_CHECKABLE)
+            }
+        }
+
 
     fun submitList(list: List<Arest>) {
         arests = list
         notifyDataSetChanged()
+    }
+
+
+    companion object {
+        private val PAYLOAD_CHECKABLE = Any()
     }
 }
