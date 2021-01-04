3acb82bbc7e29c2a406b0042e4c88ebae799abc2
Charlie-Green <vadim.churun.emp@gmail.com>
2021-01-04 17:48:53 +0200

    Server: Add SQL script to prepopulate the DB with test data

--- server/sql/create.sql
+++ server/sql/create.sql
@@ -5,7 +5,7 @@
 
 drop table if exists `Contacts`;
 drop table if exists `Periods`;
-drop table if exists `CellScheduleEntries`;
+drop table if exists `ScheduleCellEntries`;
 drop table if exists `Arests`;
 drop table if exists `Prisoners`;
 drop table if exists `Cells`;
@@ -15,22 +15,40 @@ drop table if exists `Jails`;
 -- -----------------------------------------------
 
 create table `Prisoners` (
-  `id` int not null primary key,
+  `id` int not null primary key auto_increment,
   `username` varchar(48) not null,
   `pass` blob not null,
   `name` varchar(48) not null,
-  `info` text not null
+  `info` text not null,
+
+  unique(`username`)
 );
 
 create table `Contacts` (
-  `id` int not null primary key,
   `prisoner` int not null,
-  `type` smallint,
-  `data` text
+  `type` smallint not null,
+  `data` text not null,
+
+  primary key(`prisoner`, `type`)
+);
+
+create table `Jails` (
+  `id` int not null primary key auto_increment,
+  `name` varchar(64) not null,
+  `cells` smallint not null,
+
+  unique (`name`)
+);
+
+create table `Cells` (
+  `jail` int not null,
+  `number` smallint not null,
+  `seats` smallint not null,
+  primary key (`jail`, `number`)
 );
 
 create table `Arests` (
-  `id` int not null primary key,
+  `id` int not null primary key auto_increment,
   `prisoner` int not null,
   `start` bigint not null,
   `end` bigint not null
@@ -45,23 +63,11 @@ create table `Periods` (
   primary key (`arest`, `start`, `end`)
 );
 
-create table `Cells` (
-  `jail` int not null,
-  `number` smallint not null,
-  `seats` smallint not null,
-  primary key (`jail`, `number`)
-);
-
-create table `CellScheduleEntries` (
+create table `ScheduleCellEntries` (
   `arest` int not null,
   `jail` int not null,
   `cell` smallint not null,
   primary key (`arest`, `jail`, `cell`)
-);
-
-create table `Jails` (
-  `id` int not null primary key,
-  `name` varchar(64)
 );
 
 
@@ -69,12 +75,10 @@ alter table `Contacts` add foreign key (`prisoner`) references `Prisoners` (`id`
 
 alter table `Arests` add foreign key (`prisoner`) references `Prisoners` (`id`);
 
-alter table `Periods` add foreign key (`arest`) references `Arests` (`id`);
-
-alter table `Periods` add foreign key (`jail`, `cell`) references `Cells` (`jail`, `number`);
-
 alter table `Cells` add foreign key (`jail`) references `Jails` (`id`);
 
-alter table `CellScheduleEntries` add foreign key (`arest`) references `Arests` (`id`);
+alter table `ScheduleCellEntries` add foreign key (`arest`) references `Arests` (`id`);
 
-alter table `CellScheduleEntries` add foreign key (`jail`, `cell`) references `Cells` (`jail`, `number`);
+alter table `ScheduleCellEntries` add foreign key (`jail`, `cell`) references `Cells` (`jail`, `number`);
+
+alter table `Periods` add foreign key (`arest`, `jail`, `cell`) references `ScheduleCellEntries` (`arest`, `jail`, `cell`);
---
+++ server/sql/test.sql
@@ -0,0 +1,140 @@
+-- This script prepopulates the database with script data.
+-- It should be run on a clear database
+-- (no data should have been inserted before),
+-- otherwise foreign key constraints will likely break.
+
+
+-- ---------------------------------------------
+-- Jails:
+-- Cell counts are fake.
+
+insert into Jails(`name`, `cells`)
+values("Окрестина ИВС", 3);
+
+insert into Jails(`name`, `cells`)
+values("Окрестина ЦИП", 5);
+
+insert into Jails(`name`, `cells`)
+values("Жодино", 4);
+
+
+-- Cells within Окрестина ИВС:
+
+insert into Cells(`jail`, `number`, `seats`)
+values(1, 1, 5);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(1, 2, 4);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(1, 5, 6);
+
+
+-- Cells within Окрестина ЦИП:
+
+insert into Cells(`jail`, `number`, `seats`)
+values(2, 1, 2);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(2, 2, 4);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(2, 3, 6);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(2, 4, 2);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(2, 5, 4);
+
+
+-- Cells within Жодино:
+
+insert into Cells(`jail`, `number`, `seats`)
+values(3, 1, 8);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(3, 2, 12);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(3, 3, 8);
+
+insert into Cells(`jail`, `number`, `seats`)
+values(3, 4, 10);
+
+
+-- ---------------------------------------------
+-- Profile:
+
+insert into `Prisoners`(`username`, `pass`, `name`, `info`)
+values("charl", "111", "Чарльз", "Я - тестовый пользователь.");
+
+insert into `Contacts`(`prisoner`, `type`, `data`)
+values(1, 1, "t.me/mytelega");
+
+insert into `Contacts`(`prisoner`, `type`, `data`)
+values(1, 3, "+375 44 2334455");
+
+
+-- ---------------------------------------------
+-- Arest #1:
+
+-- 14.07.2020 - 24.07.2020:
+insert into `Arests`(`prisoner`, `start`, `end`)
+values(1, 1594674000000, 1595538000000);
+
+
+-- Окрестина ИВС, 2
+insert into `ScheduleCellEntries`(`arest`, `jail`, `cell`)
+values(1, 1, 2);
+
+-- Окрестина ЦИП, 1
+insert into `ScheduleCellEntries`(`arest`, `jail`, `cell`)
+values(1, 2, 1);
+
+-- Жодино, 4
+insert into `ScheduleCellEntries`(`arest`, `jail`, `cell`)
+values(1, 3, 4);
+
+
+-- 14.07.2020 - 16.07.2020: Окрестина ИВС, 2
+insert into `Periods`(`arest`, `start`, `end`, `jail`, `cell`)
+values(1, 1594674000000, 1594846800000, 1, 2);
+
+-- 16.07.2020 - 21.07.2020: Жодино, 4
+insert into `Periods`(`arest`, `start`, `end`, `jail`, `cell`)
+values(1, 1594846800000, 1595278800000, 3, 4);
+
+-- Note: 21.07.2020 - 24.07.2020 is not specified.
+
+
+-- ---------------------------------------------
+-- Arest #2:
+
+-- 15.11.2020 - 27.11.2020:
+insert into `Arests`(`prisoner`, `start`, `end`)
+values(1, 1594674000000, 1605391200000);
+
+
+-- Окрестина ЦИП, 1:
+insert into `ScheduleCellEntries`(`arest`, `jail`, `cell`)
+values(2, 2, 1);
+
+-- Окрестина ИВС, 2:
+insert into `ScheduleCellEntries`(`arest`, `jail`, `cell`)
+values(2, 1, 2);
+
+-- Жодино, 3:
+insert into `ScheduleCellEntries`(`arest`, `jail`, `cell`)
+values(2, 3, 3);
+
+
+-- Note: 15.11.2020 - 16.11.2020 is not specified.
+
+-- 16.11.2020 - 20.11.2020: Окрестина ЦИП, 1
+insert into `Periods`(`arest`, `start`, `end`, `jail`, `cell`)
+values(2, 1594846800000, 1595278800000, 2, 1);
+
+-- 20.11.2020 - 27.11.2020: Жодино, 3
+insert into `Periods`(`arest`, `start`, `end`, `jail`, `cell`)
+values(2, 1595278800000, 1605391200000, 3, 3);
