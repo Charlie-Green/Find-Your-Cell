8747bd421b6e1ac889cc13b15ae1b125c1a5d316
Charlie-Green <vadim.churun.emp@gmail.com>
2021-01-07 18:49:17 +0200

    Android: Add and customize TabLayout to CoprisonersFragment

--- android/result/src/main/java/by/zenkevich_churun/findcell/result/ui/result/CoPrisonersFragment.kt
+++ android/result/src/main/java/by/zenkevich_churun/findcell/result/ui/result/CoPrisonersFragment.kt
@@ -8,6 +8,7 @@ import by.zenkevich_churun.findcell.core.ui.common.SviazenFragment
 import by.zenkevich_churun.findcell.core.util.android.AndroidUtil
 import by.zenkevich_churun.findcell.result.R
 import by.zenkevich_churun.findcell.result.databinding.CoprisonersFragmBinding
+import com.google.android.material.tabs.TabLayout
 
 
 class CoPrisonersFragment: SviazenFragment<CoprisonersFragmBinding>() {
@@ -17,17 +18,24 @@ class CoPrisonersFragment: SviazenFragment<CoprisonersFragmBinding>() {
     ) = CoprisonersFragmBinding.inflate(inflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupTabs()
         setupSwipeRefresh()
-
     }
 
+
+    private fun setupTabs() {
+        val tab1 = vb.tabs.newTab().apply { setText(R.string.potential_coprisoners_tabtext) }
+        val tab2 = vb.tabs.newTab().apply { setText(R.string.added_coprisoners_tabtext) }
+        vb.tabs.addTab(tab1)
+        vb.tabs.addTab(tab2)
+    }
 
     private fun setupSwipeRefresh() {
         val primaryColor = AndroidUtil.themeColor(requireContext(), R.attr.colorPrimary)
         val accentColor  = AndroidUtil.themeColor(requireContext(), R.attr.colorAccent)
-        vb.srl.setColorSchemeColors(primaryColor, accentColor)
+        vb.refreshLayout.setColorSchemeColors(primaryColor, accentColor)
 
-        vb.srl.setOnRefreshListener {
+        vb.refreshLayout.setOnRefreshListener {
             Log.v("CharlieDebug", "Refreshing! Thread is main: ${AndroidUtil.isThreadMain}")
         }
     }
