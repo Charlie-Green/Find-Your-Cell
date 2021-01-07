d9f8542b90dec50004b012bad90a9270c33362df
Charlie-Green <vadim.churun.emp@gmail.com>
2021-01-07 18:06:18 +0200

    Android: Build layout for CoPrisonersFragment

--- android/result/src/main/java/by/zenkevich_churun/findcell/result/ui/result/CoPrisonersFragment.kt
+++ android/result/src/main/java/by/zenkevich_churun/findcell/result/ui/result/CoPrisonersFragment.kt
@@ -1,7 +1,12 @@
 package by.zenkevich_churun.findcell.result.ui.result
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import by.zenkevich_churun.findcell.core.ui.common.SviazenFragment
+import by.zenkevich_churun.findcell.core.util.android.AndroidUtil
+import by.zenkevich_churun.findcell.result.R
 import by.zenkevich_churun.findcell.result.databinding.CoprisonersFragmBinding
 
 
@@ -11,5 +16,19 @@ class CoPrisonersFragment: SviazenFragment<CoprisonersFragmBinding>() {
         inflater: LayoutInflater
     ) = CoprisonersFragmBinding.inflate(inflater)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupSwipeRefresh()
 
+    }
+
+
+    private fun setupSwipeRefresh() {
+        val primaryColor = AndroidUtil.themeColor(requireContext(), R.attr.colorPrimary)
+        val accentColor  = AndroidUtil.themeColor(requireContext(), R.attr.colorAccent)
+        vb.srl.setColorSchemeColors(primaryColor, accentColor)
+
+        vb.srl.setOnRefreshListener {
+            Log.v("CharlieDebug", "Refreshing! Thread is main: ${AndroidUtil.isThreadMain}")
+        }
+    }
 }
